b054c84aaf098fe00a8861ab5e3c1891e48909e7
nicholasjayee <markmae840@gmail.com>
2025-05-15 18:17:52 +0300

    first commit

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    // Code by Ssekabira Nicholas &copy; 2025
+    // This program demonstrates the use of different data types in C.
+    // Declare  variables and asign values
+    int age = 20;
+    float height = 5.9;
+    double distance = 12345.6789;
+    char initial = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Distance: %.4lf\n", distance);
+    printf("Initial: %c\n", initial);
+
+    return 0;
+}

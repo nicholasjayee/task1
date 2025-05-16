5264dfbace3e14c06046a4c86999119cd1f331d5
nicholasjayee <markmae840@gmail.com>
2025-05-16 20:43:32 +0300

    created a foreign exhange program

--- task2-submodule/task2-branch1.c
+++ task2-submodule/task2-branch1.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
 int main()
 {
+    const int amount = 3500;
+    const float usd;
+
+    printf("Enter the amount in USD: ");
+    scanf("%f", &usd);
+
+    double exchange = (float)amount * usd;
+
+    printf("You get ugsh %.2lf when you have usd %.3f", exchange, usd);
     return 0;
 }

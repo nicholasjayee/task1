c2516b6aada2f2fdaec95e48c6780664d3436c22
nicholasjayee <markmae840@gmail.com>
2025-05-19 16:14:32 +0300

    final commit #4

--- task2-submodule/task2-branch4.c
+++ task2-submodule/task2-branch4.c
@@ -4,9 +4,14 @@ const float Tax_Rate = 0.12f;
 int main()
 {
 
+    // Code by Ssekabira Nicholas &copy; 2025
+    // local variable
     double price;
     printf("Enter price: ");
     scanf("%lf", &price);
 
     double total_price = price + (price * Tax_Rate);
+
+    printf("Total price: %.2lf\n", total_price);
+    return 0;
 }

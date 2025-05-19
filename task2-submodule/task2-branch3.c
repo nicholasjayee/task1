b96e8aca11df77dfc876334ad86e1f78a4cf13f2
nicholasjayee <markmae840@gmail.com>
2025-05-19 12:21:11 +0300

    new commit

--- task2-submodule/task2-branch3.c
+++ task2-submodule/task2-branch3.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-
+// global constant value
 const float percentage = .12f;
 int main()
 {
     // Code by Ssekabira Nicholas &copy; 2025
+
+    // local variable
     float price;
 
     printf("Enter price: ");
     scanf("%f", &price);
 
     double discounted_price = (float)price * percentage;
+
+    printf("Discounted price: %.2lf\n", discounted_price);
 
     return 0;
 }
